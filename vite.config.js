4334405fb64ab640c6b57a47a87fb708e6e5d13a
hzl(mac) <1141824193@qq.com>
2026-08-24 11:05:01 +0800

    feat: 新增支持本站入口和子路径部署配置

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 import pkg from './package.json' with { type: 'json' }
 
 export default defineConfig({
+  base: process.env.VITE_BASE_PATH || '/',
   plugins: [react()],
   define: {
     __APP_VERSION__: JSON.stringify(pkg.version),
